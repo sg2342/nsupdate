0d32ab1058d3c15c69468f88444cce11ba83ac2b
Stefan Grundmann <sg2342@googlemail.com>
2020-12-23 05:28:21 +0000

    read input from filename or standard input

--- src/nsupdate.erl
+++ src/nsupdate.erl
@@ -2,13 +2,25 @@
 -export([main/1]).
 
 main([]) ->
-    result(try nsupdate_cmd:do()
+    result(try nsupdate_cmd:do(standard_io)
 	   catch error:function_clause -> {error, invalid_input} end);
 main(["-k", KeyFile]) ->
     {ok, B} = file:read_file(KeyFile),
-    result(try nsupdate_cmd:do(nsupdate_cmd:parse_key(B))
-	   catch error:function_clause -> {error, invalid_input} end).
-
+    result(try nsupdate_cmd:do(standard_io, nsupdate_cmd:parse_key(B))
+	   catch error:function_clause -> {error, invalid_input} end);
+main([FileName]) ->
+    {ok, Fd} = file:open(FileName, [read]),
+    result(try nsupdate_cmd:do(Fd)
+	   catch error:function_clause -> {error, invalid_input} end);
+main(["-k", KeyFile, FileName]) ->
+    {ok, B} = file:read_file(KeyFile),
+    {ok, Fd} = file:open(FileName, [read]),
+    result(try nsupdate_cmd:do(Fd, nsupdate_cmd:parse_key(B))
+	   catch error:function_clause -> {error, invalid_input} end);
+main(_) ->
+    Sn = filename:basename(escript:script_name()),
+    io:format("usage: ~s [ -k keyfile] [filename]~n~n",[Sn]),
+    halt(1).
 
 result(ok) -> ok;
 result({error, _} = E) ->
--- src/nsupdate_cmd.erl
+++ src/nsupdate_cmd.erl
@@ -1,12 +1,14 @@
 -module(nsupdate_cmd).
 
--export([do/1, do/0]).
+-export([do/1, do/2]).
 -export([parse_key/1]).
 
-line() -> line(">> ").
 
-line(Prompt) ->
-    L = string:trim(io:get_line(standard_io, Prompt), trailing, "\n"),
+line(standard_io) -> line(standard_io, ">> ");
+line(Fd) -> line(Fd, "").
+
+line(Fd, Prompt) ->
+    L = string:trim(io:get_line(Fd, Prompt), trailing, "\n"),
     Tokens = lists:map(fun erlang:list_to_binary/1, string:tokens(L, " ")),
     line1(Tokens, erlang:list_to_binary(L)).
 
@@ -42,19 +44,6 @@ line1([<<"server">>, Host, Port], _) ->
     {server, {erlang:binary_to_list(Host), erlang:binary_to_integer(Port)}}.
 
 
-do(Key) ->
-    do(line(), #{ key => Key
-		, timeout => timer:seconds(300)
-		, protocol => tcp
-		, local => {any, 0}
-		, updates => [] }).
-
-do() ->
-    do(line(), #{ timeout => timer:seconds(300)
-		, protocol => tcp
-		, local => {any, 0}
-		, updates => [] }).
-
 parse_key(Bin) when is_binary(Bin) ->
     [Name |L] = string:split(string:trim(Bin), <<" ">>, all),
     [Secret| _] = lists:reverse(L),
@@ -73,15 +62,33 @@ parse_key1(Secret) ->
     {Alg, K}.
 
 
-do(send, #{ key := Key, updates := Updates, zone := Zone, server := Server}) ->
-    nsupdate_impl:query(Server, Key, Zone, lists:reverse(Updates));
-do(send, _) -> {error, missing_input};
-do({zone, Zone}, #{ } = D) -> do(line(), D#{ zone => Zone });
-do({server, Server}, #{ } = D) -> do(line(), D#{ server => Server});
-do({local, Addr, Port}, #{ } = D) -> do(line(), D#{ local => {Addr, Port}});
-do({key, Name, Secret}, #{ } = D) ->
-    {Alg, K} = parse_key1(Secret),
-    do(line(), D#{ key => {Name, Alg, K}});
-do(U, #{ updates := Updates } = D) ->
-    do(line(), D#{ updates => [U|Updates]}).
+do(Fd, Key) ->
+    do1(line(Fd), #{ key => Key
+		   , timeout => timer:seconds(300)
+		   , protocol => tcp
+		   , fd => Fd
+		   , local => {any, 0}
+		   , updates => [] }).
 
+do(Fd) ->
+    do1(line(Fd), #{ timeout => timer:seconds(300)
+		   , protocol => tcp
+		   , fd => Fd
+		   , local => {any, 0}
+		   , updates => [] }).
+
+
+do1(send, #{ key := Key, updates := Updates, zone := Zone, server := Server}) ->
+    nsupdate_impl:query(Server, Key, Zone, lists:reverse(Updates));
+do1(send, _) -> {error, missing_input};
+do1({zone, Zone}, #{ fd := Fd } = D) ->
+    do1(line(Fd), D#{ zone => Zone });
+do1({server, Server}, #{ fd := Fd} = D) ->
+    do1(line(Fd), D#{ server => Server});
+do1({local, Addr, Port}, #{ fd := Fd } = D) ->
+    do1(line(Fd), D#{ local => {Addr, Port}});
+do1({key, Name, Secret}, #{  fd := Fd } = D) ->
+    {Alg, K} = parse_key1(Secret),
+    do1(line(Fd), D#{ key => {Name, Alg, K}});
+do1(U, #{ updates := Updates, fd := Fd } = D) ->
+    do1(line(Fd), D#{ updates => [U|Updates]}).
