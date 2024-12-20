993294a8be4f2498766bacaec63ac2ace2177726
Stefan Grundmann <sg2342@googlemail.com>
2024-12-20 16:37:32 +0000

    escript: switch to argparse

--- src/nsupdate.erl
+++ src/nsupdate.erl
@@ -1,46 +1,61 @@
 -module(nsupdate).
 -export([main/1]).
 
-main([]) ->
-    result(
-        try
-            nsupdate_cmd:do(standard_io)
-        catch
-            error:function_clause -> {error, invalid_input}
-        end
-    );
-main(["-k", KeyFile]) ->
-    {ok, B} = file:read_file(KeyFile),
-    result(
-        try
-            nsupdate_cmd:do(standard_io, nsupdate_cmd:parse_key(B))
-        catch
-            error:function_clause -> {error, invalid_input}
-        end
-    );
-main([FileName]) ->
-    {ok, Fd} = file:open(FileName, [read]),
-    result(
-        try
-            nsupdate_cmd:do(Fd)
-        catch
-            error:function_clause -> {error, invalid_input}
-        end
-    );
-main(["-k", KeyFile, FileName]) ->
-    {ok, B} = file:read_file(KeyFile),
-    {ok, Fd} = file:open(FileName, [read]),
-    result(
+main(Args) ->
+    Cmd = #{
+        arguments => [
+            #{
+                name => keyfile,
+                short => $k,
+                required => false,
+                help => "keyfile"
+            },
+            #{
+                name => filename,
+                required => false,
+                help => "filename"
+            }
+        ],
+        handler => fun(A) -> result(handle(A)) end
+    },
+    argparse:run(Args, Cmd, #{progname => filename:basename(escript:script_name())}).
+
+handle(#{keyfile := KeyFile, filename := FileName}) ->
+    maybe
+        {ok, B} ?= file:read_file(KeyFile),
+        {ok, Fd} ?= file:open(FileName, [read]),
         try
             nsupdate_cmd:do(Fd, nsupdate_cmd:parse_key(B))
         catch
             error:function_clause -> {error, invalid_input}
         end
-    );
-main(_) ->
-    Sn = filename:basename(escript:script_name()),
-    io:format(standard_error, "usage: ~s [ -k keyfile] [filename]~n~n", [Sn]),
-    halt(1).
+    end;
+handle(#{keyfile := KeyFile}) ->
+    maybe
+        {ok, B} ?= file:read_file(KeyFile),
+        try
+            nsupdate_cmd:do(standard_io, nsupdate_cmd:parse_key(B))
+        catch
+            error:function_clause -> {error, invalid_input}
+        end
+    end;
+handle(#{filename := FileName}) ->
+    maybe
+        {ok, Fd} ?= file:open(FileName, [read]),
+        try
+            nsupdate_cmd:do(Fd)
+        catch
+            error:function_clause ->
+                {error, invalid_input}
+        end
+    end;
+handle(_) ->
+    try
+        nsupdate_cmd:do(standard_io)
+    catch
+        error:function_clause ->
+            {error, invalid_input}
+    end.
 
 result(ok) ->
     ok;
